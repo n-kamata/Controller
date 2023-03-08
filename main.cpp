76fe0dc0cbefbb2443810a52c0cdce29a6883830
nkamata <nobuhide.kamata@woven-planet.global>
2023-03-08 02:04:57 +0000

    Adjusted feddback gains to reduce errors.

--- main.cpp
+++ main.cpp
@@ -220,7 +220,7 @@ int main ()
   **/
   PID pid_steer = PID();
   // Adjust feedback gains.
-  pid_steer.Init(0.1, 0.001, 0.001, 1.2, -1.2);
+  pid_steer.Init(0.3, 0.001, 0.1, 1.2, -1.2);
   
   // initialize pid throttle
   /**
@@ -229,7 +229,7 @@ int main ()
   PID pid_throttle = PID();
   
   // Adjust feedback gains.
-  pid_throttle.Init(0.5, 0.001, 0.001, 1.0, -1.0);
+  pid_throttle.Init(0.2, 0.001, 0.1, 1.0, -1.0);
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
